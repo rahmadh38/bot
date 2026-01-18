212fe27f9b42bfef30ca37292db2de1b08071643
rahmadh38 <58204395+rahmadh38@users.noreply.github.com>
2026-01-18 10:53:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const {
     PermissionsBitField
 } = require('discord.js');
 
-const TARGET_BOT_ID = '1462284662213836997';
+const TARGET_WEBHOOK_ID = '1462284662213836997';
 
 const client = new Client({
     intents: [
@@ -14,29 +14,29 @@ const client = new Client({
     ],
 });
 
-let pending = new Map();
+const pending = new Map();
 
 client.once('ready', () => {
     console.log(`Login sebagai ${client.user.tag}`);
 });
 
 client.on('messageCreate', async (message) => {
-    if (message.author.bot && message.author.id !== client.user.id) return;
+    if (message.author.bot) return;
 
-    // STEP 1: minta konfirmasi
-    if (message.content === '!cleanbot') {
+    // STEP 1
+    if (message.content === '!cleanwebhook') {
         if (!message.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
             return message.reply('❌ Tidak punya izin');
         }
 
         pending.set(message.author.id, message.channel.id);
         return message.reply(
-            '⚠️ Ini akan menghapus **SEMUA pesan dari Spidey Bot (<14 hari)** di channel ini.\n' +
+            '⚠️ Ini akan menghapus **SEMUA pesan dari webhook Spidey Bot (<14 hari)** di channel ini.\n' +
             'Ketik `!confirm` untuk lanjut.'
         );
     }
 
-    // STEP 2: eksekusi
+    // STEP 2
     if (message.content === '!confirm') {
         const channelId = pending.get(message.author.id);
         if (channelId !== message.channel.id) return;
@@ -50,19 +50,19 @@ client.on('messageCreate', async (message) => {
             const msgs = await message.channel.messages.fetch({ limit: 100 });
             if (msgs.size === 0) break;
 
-            const targetMsgs = msgs.filter(m =>
-                m.author.id === TARGET_BOT_ID &&
+            const targets = msgs.filter(m =>
+                m.webhookId === TARGET_WEBHOOK_ID &&
                 Date.now() - m.createdTimestamp < 14 * 24 * 60 * 60 * 1000
             );
 
-            if (targetMsgs.size === 0) break;
+            if (targets.size === 0) break;
 
-            await message.channel.bulkDelete(targetMsgs, true);
-            total += targetMsgs.size;
+            await message.channel.bulkDelete(targets, true);
+            total += targets.size;
         }
 
         return message.channel.send(
-            `✅ Selesai. ${total} pesan dari Spidey Bot terhapus.`
+            `✅ Selesai. ${total} pesan webhook Spidey Bot terhapus.`
         );
     }
 });
